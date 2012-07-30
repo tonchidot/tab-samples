2d112d3f9eed4333f4b64a0e2e660a3074d68d27
Yukiya Takagi <yukiya@tonchidot.com>
2012-07-30 16:19:28 +0900

    added oauth2 sample for iOS

---
+++ Oauth2Sample-iOS/SampleApp/TABViewController.h
@@ -0,0 +1,13 @@
+//
+//  TABViewController.h
+//  SampleApp
+//
+//  Copyright (c) 2012年 Tonchidot Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TABViewController : UIViewController
+
+@end
+
---
+++ Oauth2Sample-iOS/SampleApp/TABViewController.m
@@ -0,0 +1,134 @@
+//
+//  TABViewController.m
+//  SampleApp
+//
+//  Copyright (c) 2012年 Tonchidot Corp. All rights reserved.
+//
+
+#import "TABViewController.h"
+
+#import "TABViewController.h"
+#import "GTMOAuth2ViewControllerTouch.h"
+#import "GTMOAuth2SignIn.h"
+#import "JSON.h"
+
+static NSString *const kTabClientId       = @"8c8be3f9ce2190dd2614c908c1701b2a";
+static NSString *const kTabClientSecret   = @"6ca23031194fc58d14bd0626f12c5da5";
+static NSString *const kKeychainItemName  = @"OAuth Sample";
+
+static NSString *const redirectUrl        = @"http://localhost:3030/test-oauth2/redirector";
+static NSString *const oauth2TokenApi     = @"https://tab.do/api/1/oauth2/token";
+static NSString *const authorizeOauth2Api = @"https://tab.do/oauth2/authorize";
+static NSString *const usersMeApi         = @"http://tab.do/api/1/users/me.json";
+
+@interface TABViewController ()
+@property (retain, nonatomic) IBOutlet UIImageView *profileImageView;
+@property (retain, nonatomic) IBOutlet UILabel *screenNameLabel;
+@property (retain, nonatomic) IBOutlet UILabel *emailLabel;
+@property (retain, nonatomic) IBOutlet UIButton *authTabButton;
+@property (nonatomic, retain) GTMOAuth2Authentication *auth;
+@end
+
+@implementation TABViewController
+@synthesize profileImageView = _profileImageView;
+@synthesize emailLabel = _emailLabel;
+@synthesize authTabButton = _authTabButton;
+@synthesize screenNameLabel = _screenNameLabel;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _profileImageView.hidden = YES;
+    _screenNameLabel.hidden = YES;
+    _emailLabel.hidden = YES;
+}
+
+- (void)viewDidUnload
+{
+    [self setScreenNameLabel:nil];
+    [self setEmailLabel:nil];
+    [self setProfileImageView:nil];
+    [self setAuthTabButton:nil];
+    [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)authTab:(UIButton *)sender {
+    [self signInToTab];
+}
+
+- (GTMOAuth2Authentication *)authForTab {
+    NSURL *tokenURL = [NSURL URLWithString:oauth2TokenApi];    
+    GTMOAuth2Authentication *auth = [GTMOAuth2Authentication authenticationWithServiceProvider:@"Custom Service Staging"
+                                                                                      tokenURL:tokenURL
+                                                                                   redirectURI:redirectUrl
+                                                                                      clientID:kTabClientId
+                                                                                  clientSecret:kTabClientSecret];
+    return auth;
+}
+
+- (void)signInToTab {
+    GTMOAuth2Authentication *auth = [self authForTab];
+    auth.scope = @"read";
+    NSURL *authURL = [NSURL URLWithString:authorizeOauth2Api];
+    
+    GTMOAuth2ViewControllerTouch *viewController;
+    viewController = [[[GTMOAuth2ViewControllerTouch alloc] initWithAuthentication:auth
+                                                                  authorizationURL:authURL
+                                                                  keychainItemName:kKeychainItemName
+                                                                          delegate:self
+                                                                  finishedSelector:@selector(viewController:finishedWithAuth:error:)] autorelease];
+    [[self navigationController] pushViewController:viewController animated:YES];
+}
+
+- (void)viewController:(GTMOAuth2ViewControllerTouch *)viewController finishedWithAuth:(GTMOAuth2Authentication *)auth error:(NSError *)error {
+    if (error != nil) {
+        // Sign-in failed
+        NSLog(@"Authentication error: %@", error);
+    } else {
+        // Sign-in succeeded
+        self.auth = auth;
+        [self getUserInfo];
+    }
+}
+
+- (void)getUserInfo {
+    _authTabButton.hidden = YES;
+    NSURL *url = [NSURL URLWithString:usersMeApi];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    GTMHTTPFetcher *fetcher = [GTMHTTPFetcher fetcherWithRequest:request];
+    self.auth.shouldAuthorizeAllRequests = YES;
+    [fetcher setAuthorizer:self.auth];
+    [fetcher beginFetchWithCompletionHandler:^(NSData *data, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error description]);
+            _authTabButton.hidden = NO;
+        } else {
+            NSString *output = [[[NSString alloc] initWithData:data
+                                                      encoding:NSUTF8StringEncoding] autorelease];
+            NSDictionary *user = [[output JSONValue] objectForKey:@"user"];
+            _screenNameLabel.text = [user objectForKey:@"screen_name"];
+            _emailLabel.text = [user objectForKey:@"email"];
+
+            NSData *dt = [NSData dataWithContentsOfURL:[NSURL URLWithString:[[user objectForKey:@"profile_image_url"] objectForKey:@"crop_M1"]]];
+            _profileImageView.image = [[UIImage alloc] initWithData:dt];
+            _screenNameLabel.hidden = NO;
+            _emailLabel.hidden = NO;
+            _profileImageView.hidden = NO;
+            _authTabButton.hidden = YES;
+        }
+    }];
+}
+
+- (void)dealloc {
+    [_screenNameLabel release];
+    [_emailLabel release];
+    [_profileImageView release];
+    [_authTabButton release];
+    [super dealloc];
+}
+@end
