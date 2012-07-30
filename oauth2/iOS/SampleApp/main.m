5da073e58c1e269dfa8a5c4a4333adc06897d793
Yukiya Takagi <yukiya@tonchidot.com>
2012-07-30 17:59:12 +0900

    add oauth2 sample for iOS

---
+++ oauth2/iOS/SampleApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleApp
+//
+//  Created by Yukiya Takagi on 12/07/27.
+//  Copyright (c) 2012年 tonchidot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TABAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TABAppDelegate class]));
+    }
+}
